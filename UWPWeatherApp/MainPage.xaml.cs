435a6f9325b3d0fc95453513a96f1c882c994c14
Gazza1996 <gkmannion@hotmail.com>
2017-03-29 17:14:42 +0100

    location bug fix

--- UWPWeatherApp/MainPage.xaml.cs
+++ UWPWeatherApp/MainPage.xaml.cs
@@ -26,8 +26,21 @@ namespace UWPWeatherApp
         public MainPage()
         {
             this.InitializeComponent();
+            
         }
-        
+
+      /*  private async void pivot(object sender, RoutedEventArgs e)
+        {
+            var location = await Location.position();
+
+            RootObject showWeather = await OpenWeatherMap.showWeather(location.Coordinate.Latitude, location.Coordinate.Longitude);
+
+            String icon = String.Format("ms-appx:///Assets/icons/{0}.png", showWeather.weather[0].icon);
+            Image.Source = new BitmapImage(new Uri(icon, UriKind.Absolute));
+
+            other.Text = "";
+        }
+        */
         private async void Page_Loaded(object sender, RoutedEventArgs e)
         {
             var location = await Location.position();
--- UWPWeatherApp/OpenWeatherMap.cs
+++ UWPWeatherApp/OpenWeatherMap.cs
@@ -15,7 +15,8 @@ namespace UWPWeatherApp
         public async static Task<RootObject> showWeather(double lat, double lon)
         {
             var http = new HttpClient();
-            var response = await http.GetAsync("http://api.openweathermap.org/data/2.5/weather?lat=53.27&lon=-9.05&appid=433a8cc464dff9ca30bee1a299a6f83a&units=metric");
+            var url = string.Format("http://api.openweathermap.org/data/2.5/weather?lat={0}&lon={1}&appid=433a8cc464dff9ca30bee1a299a6f83a&units=metric", lat, lon);
+            var response = await http.GetAsync(url);
             var result = await response.Content.ReadAsStringAsync();
             var serializer = new DataContractJsonSerializer(typeof(RootObject));
 
